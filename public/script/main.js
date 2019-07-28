4e4560df28e232fd8cfcda0c650140abad0c49c5
tomasshiao <tomasshiao2001@gmail.com>
2019-07-28 16:11:50 -0300

    initial commit

---
+++ public/script/main.js
@@ -0,0 +1,56 @@
+var app = new Vue({
+    el: "#app",
+    data:{
+        meses:[],
+        today: (new Date).getMonth() +1,
+        mesSeleccionado: {}
+    },
+    methods: {
+      calcularMes(){
+        this.mesSeleccionado = this.meses.find(x => x.numero == this.today)
+      },
+      mesAnterior (today){
+        if (today > 1){
+        app.today = app.today - 1;
+      } else {
+            app.today = app.today - 1 + 12;
+         }
+      },
+      mesPosterior (today){
+        if (today < 12){
+            app.today = app.today + 1;
+          } else {
+            app.today = app.today + 1 - 12;
+          }
+      }
+    }
+});
+
+app.meses = meses;
+app.meses.forEach(mes => {
+    mes.fechas = fechas.filter(fecha => fecha.mes == mes.numero);
+    if (mes.fechas.length > 0){
+        mes.fechas.forEach(fecha => {
+            fecha.partidos = partidos.filter(partido => partido.matchday == fecha.numero);
+    });
+    }
+});
+
+
+function cambioPagina(){
+  var schedule = document.getElementById("title1");
+  var contact = document.getElementById("title2");
+  var scheduleSection = document.getElementById("app");
+  var contactSection = document.getElementById("contact");
+  if(contact.style.display == "none"){
+    schedule.style.display =  "none";
+    scheduleSection.style.display =  "none";
+    contact.style.display =  "block";
+    contactSection.style.display =  "block";
+  } else {
+    contact.style.display =  "none";
+    contactSection.style.display =  "none";
+    schedule.style.display =  "block";
+    scheduleSection.style.display =  "block";
+  }
+}
---
+++ public/script/partidos.js
@@ -0,0 +1,229 @@
+var partidos = [{
+    matchday: 1,
+    estadio: "AJ Katzenmaier",
+    equipo1: "U1",
+    equipo2: "U4",
+    horario: "9:30",
+    map: "https://www.google.com/maps/d/viewer?mid=1QnaR8ef5aC4r4C255jypOHspc2YYhLka&ll=41.90026838780964%2C-87.62908190000002&z=18"
+},
+{
+    matchday: 1,
+    estadio: "Greenbay",
+    equipo1: "U3",
+    equipo2: "U2",
+    horario: "13:00", map: "https://www.google.com/maps/d/viewer?mid=1hZNOyJTQ-rF98ZLixGy8m37H5mvs-3fN&ll=41.9138023%2C-87.6378393&z=17"
+},
+{
+    matchday: 2,
+    estadio: "Howard A Yeager",
+    equipo1: "U5",
+    equipo2: "U6",
+    horario: "9:30",
+    map: "https://www.google.com/maps/d/viewer?mid=1wbhmRjZGOF9eRDn6oIIciuXqQd2gi_Yg&ll=41.9232646%2C-87.6629259&z=17"
+},
+{
+    matchday: 2,
+    estadio: "Majorie P Heart",
+    equipo1: "U6",
+    equipo2: "U1",
+    horario: "13:00",
+    map: "https://www.google.com/maps/d/viewer?mid=1SID7QdfDWObUWMXQOsC6PyJM3FKPEW9K&ll=41.92957828780831%2C-87.64589760000001&z=18"
+},
+{
+    matchday: 3,
+    estadio: "North",
+    equipo1: "U2",
+    equipo2: "U4",
+    horario: "9:30",
+    map: "https://www.google.com/maps/d/viewer?mid=1KZCKiaP9yNRrGkWK52RmnKaF9gy8M8xB&ll=41.9070965%2C-87.6461883&z=17"
+},
+{
+    matchday: 3,
+    estadio: "AJ Katzenmaier",
+    equipo1: "U3",
+    equipo2: "U5",
+    horario: "13:00",
+    map: "https://www.google.com/maps/d/viewer?mid=1QnaR8ef5aC4r4C255jypOHspc2YYhLka&ll=41.90026838780964%2C-87.62908190000002&z=18"
+},
+{
+    matchday: 4,
+    estadio: "South",
+    equipo1: "U1",
+    equipo2: "U3",
+    horario: "9:30",
+    map: "https://www.google.com/maps/d/viewer?mid=1xAgVyYoWCHYLd_WxuoHsMA16JfzNQASg&ll=41.9197768%2C-87.65136669999998&z=17"
+},
+{
+    matchday: 4,
+    estadio: "Howard A Yeager",
+    equipo1: "U2",
+    equipo2: "U6",
+    horario: "13:00",
+    map: "https://www.google.com/maps/d/viewer?mid=1wbhmRjZGOF9eRDn6oIIciuXqQd2gi_Yg&ll=41.9232646%2C-87.6629259&z=17"
+},
+{
+    matchday: 5,
+    estadio: "Greenbay",
+    equipo1: "U4",
+    equipo2: "U5",
+    horario: "9:30",
+    map: "https://www.google.com/maps/d/viewer?mid=1hZNOyJTQ-rF98ZLixGy8m37H5mvs-3fN&ll=41.9138023%2C-87.6378393&z=17"
+},
+{
+    matchday: 6,
+    estadio: "Majorie P Heart",
+    equipo1: "U2",
+    equipo2: "U5",
+    horario: "9:30",
+    map: "https://www.google.com/maps/d/viewer?mid=1SID7QdfDWObUWMXQOsC6PyJM3FKPEW9K&ll=41.92957828780831%2C-87.64589760000001&z=18"
+},
+{
+    matchday: 6,
+    estadio: "South",
+    equipo1:"U1",
+    equipo2: "U6",
+    horario: "13:00",
+    map: "https://www.google.com/maps/d/viewer?mid=1xAgVyYoWCHYLd_WxuoHsMA16JfzNQASg&ll=41.9197768%2C-87.65136669999998&z=17"
+},
+{
+    matchday: 7,
+    estadio: "Howard A Yeager",
+    equipo1: "U3",
+    equipo2: "U4",
+    horario: "9:30",
+    map: "https://www.google.com/maps/d/viewer?mid=1wbhmRjZGOF9eRDn6oIIciuXqQd2gi_Yg&ll=41.9232646%2C-87.6629259&z=17"
+},
+{
+    matchday: 7,
+    estadio: "Greenbay",
+    equipo1: "U5",
+    equipo2: "U1",
+    horario: "13:00",
+    map: "https://www.google.com/maps/d/viewer?mid=1hZNOyJTQ-rF98ZLixGy8m37H5mvs-3fN&ll=41.9138023%2C-87.6378393&z=17"
+},
+{
+    matchday: 8,
+    estadio: "North",
+    equipo1: "U6",
+    equipo2: "U3",
+    horario: "9:30",
+    map: "https://www.google.com/maps/d/viewer?mid=1KZCKiaP9yNRrGkWK52RmnKaF9gy8M8xB&ll=41.9070965%2C-87.6461883&z=17"
+},
+{
+    matchday: 8,
+    estadio: "Majorie P Heart",
+    equipo1: "U2",
+    equipo2: "U4",
+    horario: "13:00",
+    map: "https://www.google.com/maps/d/viewer?mid=1SID7QdfDWObUWMXQOsC6PyJM3FKPEW9K&ll=41.92957828780831%2C-87.64589760000001&z=18"
+},
+{
+    matchday: 9,
+    estadio: "AJ Katzenmaier",
+    equipo1: "U3",
+    equipo2: "U1",
+    horario: "9:30",
+    map: "https://www.google.com/maps/d/viewer?mid=1QnaR8ef5aC4r4C255jypOHspc2YYhLka&ll=41.90026838780964%2C-87.62908190000002&z=18"
+},
+{
+    matchday: 9,
+    estadio: "Howard A Yeager",
+    equipo1: "U5",
+    equipo2: "U6",
+    horario: "13:00",
+    map: "https://www.google.com/maps/d/viewer?mid=1wbhmRjZGOF9eRDn6oIIciuXqQd2gi_Yg&ll=41.9232646%2C-87.6629259&z=17"
+}];
+
+var fechas = [{
+    numero: 1,
+    dia: 1,
+    mes: 9,
+    partidos: []
+},
+{
+    numero: 2,
+    dia: 8,
+    mes: 9,
+    partidos: []
+},
+{
+    numero: 3,
+    dia: 15,
+    mes: 9,
+    partidos: []
+},
+{
+    numero: 4,
+    dia: 22,
+    mes: 9,
+    partidos: []
+},
+{
+    numero: 5,
+    dia: 29,
+    mes: 9,
+    partidos: []
+},
+{
+    numero: 6,
+    dia: 6,
+    mes: 10,
+    partidos: []
+},
+{
+    numero: 7,
+    dia: 13,
+    mes: 10,
+    partidos: []
+},
+{
+    numero: 8,
+    dia: 20,
+    mes: 10,
+    partidos: []
+},
+{
+    numero: 9,
+    dia: 27,
+    mes: 10,
+    partidos: []
+}];
+
+var meses = [
+    {nombre: "January",
+    numero: 1,
+    fechas: []},
+    {nombre: "February",
+     numero: 2,
+     fechas: []},
+     {nombre: "March",
+     numero: 3,
+     fechas: []},
+     {nombre: "April",
+     numero: 4,
+     fechas: []},
+     {nombre: "May",
+     numero: 5,
+     fechas: []},
+     {nombre: "June",
+     numero: 6,
+     fechas: []},
+     {nombre: "July",
+     numero: 7,
+     fechas: []},
+     {nombre: "August",
+     numero: 8,
+     fechas: []},
+     {nombre: "September",
+     numero: 9,
+     fechas: []},
+     {nombre: "October",
+     numero: 10,
+     fechas: []},
+     {nombre: "November",
+     numero: 11,
+     fechas: []},
+     {nombre: "December",
+     numero: 12,
+     fechas: []}
+];
